9acdd47c28c3d1a7a2f18a2677d97c6fe4027399
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-03-22 15:12:21 +0200

    auto-message.

--- bl/users.rb
+++ bl/users.rb
@@ -11,30 +11,33 @@ def get_users(crit = {}, opts = {})
   {users: users}
 end
 
+# GET http://okfounder.herokuapp.com/users
 get '/users' do
   crit = {}
   get_users(crit)
 end
 
+# GET http://okfounder.herokuapp.com/users/search?query=jeru
 get '/users/search' do
   query = params[:query].to_s
   crit = crit_any_field($users,query)
   get_users(crit)
 end
 
+# POST "name=David" http://okfounder.herokuapp.com/users/create
 post '/users/create' do
   data = params.just(WHITE_USER_PARAMS)
   data[:code] = SecureRandom.uuid
   create_user(data)
 end
 
+# curl -d "code=d873c798-860c-4293-acc1-ae0f06429c7f&desc=currently lives in Jerusalem" http://okfounder.herokuapp.com/users/update
 post '/users/update' do
   set_data = params.just(WHITE_USER_PARAMS)
-  $users.update({code: params[:code]},set_data)
+  $users.update_one({code: params[:code]},set_data)
 end
 
-
-# http://localhost:9292/users/HaSlQ6nrvg
+# GET http://okfounder.herokuapp.com/users/xUP4CYOAPg
 get '/users/:id' do
   get_users(_id: params[:id])
 end
